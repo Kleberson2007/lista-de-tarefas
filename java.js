6dd21328d5fd67d48cbb5daa05fcaee9df1ed426
Kleberson dos Santos Mendes Pinto <klebersonsantosmp@gmail.com>
2024-06-04 16:30:16 -0300

    1

--- java.js
+++ java.js
@@ -1,11 +1,11 @@
-function add() {
+function add () {
     const ul = document.querySelector("ul")
-    const input = document.querySelector("imput")
+    const input = document.querySelector("input")
     const tarefa = input.value
-    
+
     const li = document.createElement("li")
     li.innerText = tarefa
     ul.appendChild(li)
-    
-    input.value = "a"
+
+    input.value = ""
 }
